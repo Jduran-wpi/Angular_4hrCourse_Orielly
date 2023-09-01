0430e04057f6651f4c76bb5abac86ca8d3b7d64d
Jduran-wpi <87102538+Jduran-wpi@users.noreply.github.com>
2023-09-01 15:06:56 -0400

    Delete 04-ViewTechniques/DemoApp directory

--- 04-ViewTechniques/DemoApp/src/app/flexi-title-case.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FlexiTitleCasePipe } from './flexi-title-case.pipe';
-
-describe('FlexiTitleCasePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FlexiTitleCasePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
